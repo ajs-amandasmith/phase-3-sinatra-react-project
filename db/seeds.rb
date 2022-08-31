56726ebd865b0b82701ba77f2bf9873102629ea4
Amanda Smith <ajs.amandasmith@gmail.com>
2022-08-31 16:21:24 -0500

    Adds seed data and Faker to the Gemfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,24 @@
-puts "🌱 Seeding spices..."
+puts "🌱 Seeding books..."
 
 # Seed your database here
+
+# run a loop 10 times
+10.times do
+  # create a random author
+  author = Author.create(
+    name: Faker::Book.unique.author
+  )
+
+  # create between 1 and 5 books for each author
+  rand(1..5).times do
+    Book.create(
+      title: Faker::Book.unique.title,
+      genre: Faker::Book.genre,
+      publisher: Faker::Book.publisher,
+      author_id: author.id
+    )
+  end
+
+end
 
 puts "✅ Done seeding!"
